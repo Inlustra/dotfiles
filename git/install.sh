52cc3b39d3e11238e17bdcc6af04e8a11a56fea7
Thomas Nairn <thomas@thenairn.com>
2020-05-30 15:24:36 +0100

    Add hub install for ubuntu

--- git/install.sh
+++ git/install.sh
@@ -1,1 +1,27 @@
 git config core.hooksPath git/hooks/
+
+
+
+aptInPath() {
+  if [[ -n $ZSH_VERSION ]]; then
+    builtin whence -p "apt-get" &> /dev/null
+  else  # bash:
+    builtin type -P "apt-get" &> /dev/null
+  fi
+}
+
+hubInPath() {
+  if [[ -n $ZSH_VERSION ]]; then
+    builtin whence -p "hub" &> /dev/null
+  else  # bash:
+    builtin type -P "hub" &> /dev/null
+  fi
+}
+
+if hubInPath; then
+    echo "hub already installed"
+else
+    if aptInPath; then
+        apt-get install -y hub 
+    fi;
+fi
