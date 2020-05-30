d5c8dcad0f99a84e25ee3fbaea312eb1099a9539
Thomas Nairn <thomas@thenairn.com>
2020-05-30 15:16:58 +0100

    Skip vscode link when not on mac

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,2 +1,7 @@
+
 ZSH=$HOME/.dotfiles
-$ZSH/bin/link-file  "$HOME/.vscode.conf" "$HOME/Library/Application Support/Code/User"
+if [ "$(uname)" == "Darwin" ]; then
+    $ZSH/bin/link-file  "$HOME/.vscode.conf" "$HOME/Library/Application Support/Code/User"
+else
+    $ZSH/bin/log_user "Skipping vscode linking as not on mac."
+fi
